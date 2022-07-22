bc0e73c25e77f1f00a3b3687dedf101f92eb34dc
dev-freitas <98091528+dev-freitas@users.noreply.github.com>
2022-07-22 17:28:19 +0100

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_SOTORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+    apiKey: "AIzaSyBtlaB-XyZHXLlDmu0jUIQQREJTu5SHm-k",
+    authDomain: "contact-kepper-dev.firebaseapp.com",
+    projectId: "contact-kepper-dev",
+    storageBucket: "contact-kepper-dev.appspot.com",
+    messagingSenderId: "578257422472",
+    appId: "1:578257422472:web:5bc84df8f7e1de86f0f73d"
 };
 
 // Initialize Firebase
